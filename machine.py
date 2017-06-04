479fff79f3ba3dc8c6eb48c2a15d21e4f07858e7
atsavin <atsavin.halliem@gmail.com>
2017-06-05 01:06:03 +0700

    update machine hampir fix

--- machine.py
+++ machine.py
@@ -8,64 +8,35 @@ import sys
 import Pyro4
 from Pyro4.util import SerializerBase
 
-state = "E:/"
-host = "10.151.62.36"
-port_pyro= 9090
+state = "Folder1/"
+host = "10.151.43.75"
+port_pyro= 9091
 pyroname="machine2"
-server = Pyro4.core.Proxy("PYRO:example.dc.dispatcher@10.151.62.36:9096")
+server = Pyro4.core.Proxy("PYRO:example.dc.dispatcher@10.151.43.75:9096")
 
 @Pyro4.expose
 class Machine(object):
-
-
-    #def main():
-        #getState()
-    #    server = Pyro4.core.Proxy("PYRONAME:server")
-    #    WORKERNAME = "Worker_%d@%s" % (os.getpid(), socket.gethostname())
-    #    print(WORKERNAME)
-        #state = "E:/"
-        #file="E:/test.txt"
-        #touch(state,file)
-        #cp(file,"F:/")
-        #rm(state,file)
-        #ls("F:/")
-        #index=10
-        ##word=[{'machine':'arya','path':'arya'},{'machine':'rijal','path':'rijal'}]
-        ##word.append({'machine':'arya','path':'arya'})
-        #print word[2]['machine']
-        #print(getData("E:/logs.txt"))
-        #file = open("E:/its.py",'w')
-        #for item in getData("E:/test.py"):
-        #    file.write(item)
-        #file.close
-        #file=getData("E:/logs.txt")
-        #file2=open("log.txt",'w')
-        #file2=file
-        #file2.close()
-        #file.close()
-        #file.write(getData("E:/logs.txt"))
 
     def __init__(self):
 
         val=self.pyro()
         print(val)
         server.getPyroname(val)
-        #print(val[0]['path'])
-        #print(self.pyro())
 
-    def touch(file):
+    def touch(self,file):
         open(state+file,'a').close()
 
-    def rm(file):
-        os.remove(file)
+    def save(self, file, name):
+        copied_file = open(state+name,'a')
+	    copied_file.write(file)
+	    copied_file.close()
 
-    def getList(self,state):
-        #mypath = "E:/"
-        #ls = []
+    def rm(self,file):
+        os.remove(state+file)
+
+    def getList(self):
         onlyfiles = [f for f in listdir(state) if isfile(join(state,f)) ]
         return onlyfiles
-        #for item in onlyfiles:
-            #print(item)
 
     def getState():
         print(state)
@@ -73,26 +44,19 @@ class Machine(object):
     def setState(path):
         state=path
 
-    def getData(path):
-        file = open(path,'r')
+    def getData(self,path):
+        file = open(state+path,'r')
         sentfile = file.read()
         return sentfile
 
     def pyro(self):
-        #server.setUri(pyroname+"@"+host+":"+port)
         val = ({"machine":pyroname+"@"+host+":"+str(port_pyro),"path":state})
         return val
-        #return pyroname+"@"+host+":"+port
 
 if __name__ == "__main__":
     Pyro4.config.HOST = host
-    #Pyro4.config.PORT = port
-    #Pyro4.config.SERVERTYPE = "thread"
     Pyro4.Daemon.serveSimple(
         {
             Machine(): pyroname
         }, verbose=True, ns=False, port=port_pyro
     )
-    #getPyroname()
-
-    #main()
